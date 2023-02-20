2f87d4aed1271315d742daacbcdde0e9b4067715
Estah Ndunge <n.manguu@gmail.com>
2023-02-20 22:09:00 +0300

    add an endpoint to save a book in the database

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const userModel = require('./../models/users');
+const bookModel = require('./../models/books');
+
 
 router.post('/', async function (req, res) {
     const data = new userModel(
@@ -17,6 +19,24 @@ router.post('/', async function (req, res) {
         }
         catch (error) {
             res.status(404).json({message: error.message});
+
+        }
+   
+});
+
+router.post('/add-book', async function (req, res) {
+    const data = new bookModel(
+        {
+            title: req.body.title,
+            author: req.body.author,
+            publisher: req.body.publisher
+        });
+        try {
+            const dataEntered = await data.save();
+            res.status(200).json(dataEntered);
+        }
+        catch (error) {
+            res.status(500).json({message: error.message});
 
         }
    
